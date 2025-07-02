16ef54dcc95d2f84a2c52157ab16cfc4eb0fe1b7
João Mateus <141691766+joaomgsb@users.noreply.github.com>
2025-07-01 22:35:08 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/servicos" element={<ServicesPage />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
