6aeb91440c8a40a526db6b10a305ce1bfb55e586
João Mateus <141691766+joaomgsb@users.noreply.github.com>
2025-07-01 22:34:56 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import { ServicesPage } from "./pages/ServicesPage";
 
 const queryClient = new QueryClient();
 
